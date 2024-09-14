22f9e7ee154ca467432d636269ba77bf80e4f203
Sourabh <lordgk02@gmail.com>
2024-09-14 08:19:15 +0530

    masked text added

--- src/pages/PlayGame.jsx
+++ src/pages/PlayGame.jsx
@@ -1,9 +1,43 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import { useState } from "react";
+import { Maskedtext } from './../components/maskedText/Maskedtext';
+import Keyboard from './../components/keyboard/Keyboard';
+
+
+
+
 
 function PlayGame() {
+    const location= useLocation()
+    const word=location.state.value
+    const [guessedLetters, setGuessedLetters] = useState([]);
+    const [step, setStep] = useState(0);
+
+    function handleLetterClick(letter) {
+        if(word.toUpperCase().includes(letter)) {
+            console.log('Correct');
+        } else {
+            console.log('Wrong');
+            setStep(step + 1);
+        }
+
+        setGuessedLetters([...guessedLetters, letter]);
+    }
+    
     return (
         <>
-            <h1>Play Game</h1>
+            <h1>Guess the word</h1>
+            <Maskedtext
+            orignalWord={word}
+            guessedletters={guessedLetters}
+            />
+            <br />
+          <div>
+            <Keyboard
+            guessedword={guessedLetters}
+            orignalword={word}
+            onletterclick={handleLetterClick}/>
+          </div>
             <Link to='/start'  className="text-blue-400">Start Game Link</Link>
         </>
     );
